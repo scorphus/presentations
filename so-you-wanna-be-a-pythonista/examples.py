5699b556203b443d418576704cc055d7d95ed607
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2018-06-28 01:51:19 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! Add so-you-wanna-be-a-pythonista presentation

---
+++ so-you-wanna-be-a-pythonista/examples.py
@@ -0,0 +1,104 @@
+num_periods = len(traded_per_period_kwh)
+predicted_soc = [current_soc]
+
+for i in range(1, num_periods + 1):
+    prediction = predicted_soc[i - 1] - traded_per_period_kwh[i - 1] / active_capacity_kwh
+    prediction = clip_rate(prediction)
+    predicted_soc.append(prediction)
+
+items = ['foo', 'bar', 'spam', 'egg']
+
+for i in range(len(items)):
+    print(items[i])
+
+
+value = tuple(range(10))
+
+other = (value[i] for i in range(len(value) - 1, -1, -1))
+return tuple(other))
+
+
+value = tuple(range(10))
+
+return tuple(reversed(value))
+
+
+
+self.data.sort(lambda b, a: cmp(getattr(a, crit),
+                                getattr(b, crit)))
+for e in self.data:
+    if e.calls:
+        # ...
+
+
+result = []
+f = open(some_file, 'r')
+while True:
+    l = f.readline()
+    if l == '':
+        break
+    l = l.rstrip()
+    result.append(l)
+f.close()
+
+
+
+# WTF
+def parse_data(data):
+    retval = None
+    for _, vals in list_of_parsers.items():
+        regexp = vals['regexp']
+        parser = vals['parser']
+        if re.match(regexp, data):
+            retval = parse_func(data, parser)
+            break
+    if not retval:
+        raise ValueError('Data could not be parsed')
+    return retval
+
+
+def parse_data(data):
+    for vals in list_of_parsers.values():
+        regexp, parser = vals['regexp'], vals['parser']
+        if re.match(regexp, data):
+            return parse_func(data, parser)
+    raise ValueError('Data could not be parsed')
+
+
+
+
+ok = False
+for i in range(some_index-1, -1, -1):
+    some_num = some_dict.get(ord(some_list[i]))
+    if some_num == ord('T'):
+        continue
+    if some_num in [ord('L'), ord('D')]:
+        ok = True
+        break
+
+if not ok:
+    return False
+
+ok = False
+for i in range(some_index+1, len(some_list)):
+    some_num = some_dict.get(ord(some_list[i]))
+    if some_num == ord('T'):
+        continue
+    if some_num in [ord('R'), ord('D')]:
+        ok = True
+        break
+return ok
+
+
+
+# WTF
+if cp_value == 0x200d:
+
+    if pos > 0:
+        if _combining_class(ord(label[pos - 1])) == _virama_combining_class:
+            return True
+    return False
+
+else:
+
+    return False
